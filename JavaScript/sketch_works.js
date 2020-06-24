8501c9689a7c222c142055f3b4fc2a7277fad815
Mayuko <60499285+crymayuko98@users.noreply.github.com>
2020-06-25 01:27:55 +0900

    Add files via upload

---
+++ JavaScript/sketch_works.js
@@ -0,0 +1,17 @@
+function setup() {
+  colorMode(HSL,360,100,100,100);
+  canvas = createCanvas(windowWidth,document.body.clientHeight);
+     canvas.position(0,0);
+     canvas.style('z-index','-1');
+     background(100);
+ }
+ 
+ function draw() {
+   fill(random(130,180), 85, 70,25);
+   noStroke();
+   ellipse(random(windowWidth), random(document.body.clientHeight), random(150));
+   if(frameCount > 180){
+     noLoop();
+   }
+ }
+ 
